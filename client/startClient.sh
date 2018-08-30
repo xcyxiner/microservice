2896e1e6a837095432cb3346101c9d382dddde22
xcyxiner <593091948@qq.com>
2018-08-30 18:25:45 +0800

    consul 改为 -bootstrap-expect 2,启动2个时就可以选出leader

--- client/startClient.sh
+++ client/startClient.sh
@@ -2,3 +2,5 @@ sh clientConsul.sh
 sh clientRegistrator.sh
 sh clientSwarm.sh
 sh clientWeb.sh
+sh clientLb.sh
+sh clientKeepalived.sh
--- master/startMaster.sh
+++ master/startMaster.sh
@@ -3,3 +3,4 @@ sh masterRegistrator.sh
 sh masterSwarm.sh
 sh masterWeb.sh
 sh masterLb.sh
+sh masterKeepalived.sh
